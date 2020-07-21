b838981a5f459e41c65569297e2e103e95cac864
yusuf30stack <68436752+yusuf30stack@users.noreply.github.com>
2020-07-21 14:04:58 +0100

    Add files via upload

---
+++ project2/script/script.js
@@ -0,0 +1,81 @@
+
+/* FILTERS LESSON */
+    
+/*
+$("header nav li:first").css({border: "2px solid red"});
+$("header nav li:last").css({border: "2px solid red"}); 
+*/
+
+/*
+$("#contact ul:first").css({border: "2px solid green"});
+$("#contact ul:last").css({border: "2px solid green"});
+*/
+
+/*
+$("header nav li:even").css({border: "2px solid blue"});
+*/
+
+/*
+$("header nav li:odd").css({border: " 2px solid yellow"});
+*/
+/*
+$("header nav li:odd").css({border: " 2px solid yellow"});
+*/
+
+/*
+$("section:not('#contact')").css({border: " 2px solid green"});
+*/
+
+/*
+$("#social-nav li:lt(3)").css({border: " 2px solid blue"});
+*/
+
+/*
+$("#social-nav li:gt(2)").css({border: " 2px solid blue"});
+*/
+
+/*
+$("div[class]").css({border: " 2px solid pink"});
+*/
+
+/*
+$("img[alt=quote]").css({border: " 2px solid purple"});
+*/
+
+
+
+
+/****** methods ********/
+
+ /*  
+$("#contact-methods").next().css({border: "3px solid blue"}); */
+
+/*
+$("#social-nav").prev().css({border: "3px solid red"}); */
+/*
+$(".banner-title").parent().css({border: "3px solid green"}); */
+/*
+$("#social-nav").children().css({border: "3px solid pink"}); */
+/*
+$("#lead-banner").find(".banner-title").css({border: "3px solid purple"});*/
+/*
+$("#social-nav").closest(".wrapper").css({border: "3px solid orange"}); */
+
+/****** Chaning *******/
+
+/*
+$("#contact-methods").css({border: "2px solid red"})
+    .next().css({border: "2px solid green"})
+    .closest("section").css({border: "2px solid blue"}); */
+
+/******addding new contecnt******/
+
+/*
+var tweet = "he big fight live:ham vs cheese:";
+
+$("#tweets div p").text(tweet);
+*/
+
+/*****wrapping&unwrapping******/
+
+$("section").wrap("<div>");
